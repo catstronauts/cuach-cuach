ac742475ecfd116c98b911bb6466f64db8cafaea
Aitor <aitorpalomares@hotmail.com>
2020-12-03 17:46:59 +0000

    footer include social icons

--- src/ui/layouts/Footer/Footer.js
+++ src/ui/layouts/Footer/Footer.js
@@ -1,12 +1,35 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import icon from '@assets/images/icon.png';
+import { Container } from '@layouts/Container';
+import { AppContext } from '@layouts';
+import { IconSocial } from '@components/Icon';
 import styles from './Footer.module.scss';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-   <p>Cuach Cuach</p>
+const SocialLinks = ({ social = [] }) => social.length ? (
+  <ul className={styles.socialLinks}>
+    {social.map((sm = {}, i) => (
+      <li key={i} className={styles.socialLinks_item}>
+        <a href={sm.url} className={styles.socialLinks_link} target="_black">
+          <IconSocial name={sm.type} />
+        </a>
+      </li>
+    ))}
+  </ul>
+) : null;
 
-  </footer>
-);
+const Footer = () => {
+  const { socialLinks = [] } = useContext(AppContext);
 
+  return (
+    <footer className={styles.footer}>
+      <Container className={styles.content}>
+        <span className={styles.companyDetails}>
+          <img className={styles.img} src={icon} alt='Cuach Cuach logo' />
+          Cuach Cuach
+        </span>
+        <SocialLinks social={socialLinks} />
+      </Container>
+    </footer>
+  );
+};
 export default Footer;
