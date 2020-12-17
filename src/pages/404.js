37cadc9c71d72cd36c61d65fdb9c0b0133e53081
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2020-12-17 13:00:44 +0000

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Layout from '@layouts';
-import { Section } from '@layouts/Section';
-import styles from './blog.module.scss';
+// import { Section } from '@layouts/Section';
+import Button from '@ui/components/Button';
+import styles from './404.module.scss';
+import img1 from '@assets/images/404_1.png';
+import img2 from '@assets/images/404_2.png'
 
 const NotFound = ({ data = {} }) => (
   <Layout>
-    <div className={styles.hero}>404</div>
-    <Section title='No results found 😞'>
+    <div className={styles.page404}>
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h4 className={styles.title}><span className={styles.title404}>404</span><br />Página no encontrada</h4>
+          <p className={styles.paragraph}>Ups! Parece que ésta página no existe.</p>
+          <Button caption='Volver al Inicio' />
+        </div>
+        <div className={styles.imagesWrapper}>
+          <div className={styles.images}>
+            <img className={styles.img1} src={img1}/>
+            <img className={styles.img2} src={img2} /> 
+          </div>
+        </div>
+      </div>
+    </div>
+    {/* <Section title='No results found 😞'>
       not found
-    </Section>
+    </Section> */}
   </Layout>
 );
 
