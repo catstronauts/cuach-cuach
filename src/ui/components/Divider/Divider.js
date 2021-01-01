5e20aef88f1f138b5a38ccbcf3128fbd6eb9dd55
Aitor <aitorpalomares@hotmail.com>
2021-01-01 18:39:02 +0000

    add Divider as division bar

--- src/ui/components/Divider/Divider.js
+++ src/ui/components/Divider/Divider.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Divider.module.scss';
 
-const Divider = ({ className }) => (
+const Divider = ({ className, themed = false }) => (
   <hr
     className={classnames(
       styles.divider,
+      themed && styles.themed,
       className
     )}
   />
