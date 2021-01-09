6fe315d1daeaa82d554f3ec28d3438f0e2fca22f
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-01-09 18:41:00 +0000

    created category component

--- src/ui/pages/GalleryPage/GalleryPage.js
+++ src/ui/pages/GalleryPage/GalleryPage.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import get from 'lodash/get';
-// import { Section } from '@layouts/Section';
+import { Link } from '@components/Link';
+import { LINKS } from '@src/constants';
 import CardProduct from '@components/CardProduct';
 import styles from './GalleryPage.module.scss';
+
+const Category = ({ to, text }) => (
+  <Link to={to}>
+    <p className={styles.category}>{text}</p>
+  </Link>
+);
 
 const GalleryPage = ({ data, title }) => {
   const products = get(data, 'allContentfulProduct.edges');
@@ -16,9 +23,35 @@ const GalleryPage = ({ data, title }) => {
           <div className={styles.categoryMenu}>
             <p className={styles.categoryTitle}>Categorías:</p>
             <div className={styles.categories}>
-              <p className={styles.category}>Manufactura</p>
-              <p className={styles.category}>Arte</p>
-              <p className={styles.category}>Arquitectura</p>
+              <Category
+                to={LINKS.GALLERY.to}
+                text={LINKS.GALLERY.text}
+              />
+              <Category
+                to={LINKS.GALLERY_TEXTIL.to}
+                text={LINKS.GALLERY_TEXTIL.text}
+              />
+              <Category
+                to={LINKS.GALLERY_ARTE.to}
+                text={LINKS.GALLERY_ARTE.text}
+              />
+              <Category
+                to={LINKS.GALLERY_ARQUITECTURA.to}
+                text={LINKS.GALLERY_ARQUITECTURA.text}
+              />
+
+              {/* <Link to={LINKS.GALLERY.to}>
+                <p className={styles.category}>{LINKS.GALLERY.text}</p>
+              </Link>
+              <Link to={LINKS.GALLERY_TEXTIL.to}>
+                <p className={styles.category}>{LINKS.GALLERY_TEXTIL.text}</p>
+              </Link>
+              <Link to={LINKS.GALLERY_ARTE.to}>
+                <p className={styles.category}>{LINKS.GALLERY_ARTE.text}</p>
+              </Link>
+              <Link to={LINKS.GALLERY_ARQUITECTURA.to}>
+                <p className={styles.category}>{LINKS.GALLERY_ARQUITECTURA.text}</p>
+              </Link> */}
             </div>
           </div>
           <div className={styles.productsCards}>
