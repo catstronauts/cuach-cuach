d35b2ac41c0c70c00315ce549c9400849c885d27
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-01-10 15:41:19 +0000

    created product page

--- src/templates/gallery-article.js
+++ src/templates/gallery-article.js
@@ -4,26 +4,34 @@ import get from 'lodash/get';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Helmet } from '@layouts/Helmet';
 import { Section } from '@layouts/Section';
-import Hero from '@components/Hero';
+// import Hero from '@components/Hero';
+import BackgroundImage from 'gatsby-background-image';
 import styles from './gallery-article.module.scss';
 
-const BlogPostTemplate = (props) => {
+const ProductPage = (props) => {
   const post = get(props, 'data.contentfulProduct', {});
   const { images = [], title } = post;
 
   return (
     <>
       <Helmet pageTitle={title} />
-      <Hero img={images[0].fluid} imgAlt={title}/>
+      
       <Section>
-        <h1 className={styles.title}>{post.title}</h1>
-        {documentToReactComponents(post.body.json)}
+        <div className={styles.productPage}>
+          <div className={styles.images}>
+            <BackgroundImage className={styles.mainImage} fluid={images[0].fluid} imgAlt={title}/>
+          </div>
+          <div className={styles.text}>
+            <h1 className={styles.title}>{post.title}</h1>
+            {documentToReactComponents(post.body.json)}
+          </div>
+        </div>
       </Section>
     </>
   );
 };
 
-export default BlogPostTemplate;
+export default ProductPage;
 
 export const pageQuery = graphql`
   query GalleryArticleBySlug($slug: String!) {
