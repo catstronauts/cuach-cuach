b4fafc0132e828f9c1e7785050c1c84780f6392a
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-01-19 19:39:53 +0000

    added margins

--- src/ui/pages/home/Service/Service.js
+++ src/ui/pages/home/Service/Service.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Img from 'gatsby-image';
+import { Icon } from '@components/Icon';
 import styles from './Service.module.scss';
 
 
-const ServiceCard = () => (
-  <div class="card">
-    <div class="icon"><i class="fas fa-truck"></i></div>
-    <div class="text">
-      <h5><strong>FREE SHIPPING ON ORDERS OVER €75</strong></h5>
-      <p>Deliver only in the Republic of Ireland</p>
+const ServiceCard = ({ iconName, title, paragraph }) => (
+  <div className={styles.card}>
+    <div className={styles.text}>
+      <Icon className={styles.icon} name={iconName} />
+      <h5 className={styles.title}><strong>{title}</strong></h5>
+      <p className={styles.paragraph}>{paragraph}</p>
     </div>
   </div>
 );
@@ -16,7 +16,23 @@ const ServiceCard = () => (
 
 const Service = () => (
   <div className={styles.service}>
-
+    <div className={styles.serviceContent}>
+      <ServiceCard 
+        iconName='shopping-basket' 
+        title='Compras a traves de nuestro correo.'
+        paragraph='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+      />
+      <ServiceCard 
+        iconName='shipping-fast' 
+        title='Hacemos envíos a todo Chile'
+        paragraph='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+      />
+      <ServiceCard 
+        iconName='hand-holding-heart' 
+        title='100% hecho a mano'
+        paragraph='Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+      />
+    </div>
   </div>
 );
 
