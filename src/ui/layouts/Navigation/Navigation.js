af250f65f139121b57d0dfc553fb2f9897790089
Aitor <aitorpalomares@hotmail.com>
2021-01-20 18:02:30 +0000

    remove console.logs

--- src/ui/layouts/Navigation/Navigation.js
+++ src/ui/layouts/Navigation/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { useAppContext } from '@layouts';
 import { LINKS } from '@src/constants';
 import { NavigationBar } from './NavigationBar';
 import { SideMenu } from './SideMenu';
@@ -12,20 +11,11 @@ const menuItems = [
   LINKS.CONTACT_US,
 ];
 
-const Navigation = () => {
-  // const { windowProps = {} } = useAppContext();
-  // const { isDesktop } = windowProps;
-  // console.log('Navigation', isDesktop);
-  // console.log(windowProps);
-
-  return (
-    <>
-      <SideMenu options={menuItems} className={styles.mob} />
-      <NavigationBar options={menuItems} className={styles.desk} />
-      {/* <NavigationBar options={menuItems} className={!isDesktop && 'hidden'} />
-      <SideMenu options={menuItems} className={isDesktop && 'hidden'} /> */}
-    </>
-  );
-};
+const Navigation = () => (
+  <>
+    <SideMenu options={menuItems} className={styles.mob} />
+    <NavigationBar options={menuItems} className={styles.desk} />
+  </>
+);
 
 export default Navigation;
