8eaa3f7e13462a04b1e249d6abb65b59586ed677
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-01-21 23:41:02 +0000

    pagination styling

--- src/ui/components/Pagination/Pagination.js
+++ src/ui/components/Pagination/Pagination.js
@@ -5,29 +5,29 @@ import { Button } from '@components/Button';
 import styles from './Pagination.module.scss';
 
 const Pagination = ({ path, currentPage, limit, numPages, className }) => {
-  const linkPrev =  (currentPage == 2)
+  const linkPrev =  (currentPage === 2)
     ? `${path}`
     : `${path}/${currentPage - 1}`;
   const linkNext = `${path}/${currentPage + 1}`;
-  const disablePrev = (currentPage == 1)
+  const disablePrev = (currentPage === 1)
     ? true
     : false;
-  const disableNext = (currentPage == numPages)
+  const disableNext = (currentPage === numPages)
     ? true
     : false;
 
 
-  return (numPages != 1) ? (
+  return (numPages !== 1) ? (
     <div 
       className={classnames(
         styles.pagination,
         className
     )}>
-      <Link to={linkPrev}>
-        <Button disabled={disablePrev} caption='<'/>
+      <Link className={styles.link} to={linkPrev}>
+        <Button className={styles.btn} disabled={disablePrev} caption='<'/>
       </Link>
-      <Link to={linkNext}>
-        <Button disabled={disableNext} caption='>' />
+      <Link className={styles.link} to={linkNext}>
+        <Button className={styles.btn} disabled={disableNext} caption='>' />
       </Link>
     </div>
   ) : null;
