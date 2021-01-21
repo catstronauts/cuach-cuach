db47ac85076a1770bf6aa7838e0f4106a6c3acb4
Elisa Munoz <52674719+elisamunoz@users.noreply.github.com>
2021-01-21 17:44:25 +0000

    added logic to pagination buttons

--- src/ui/components/Pagination/Pagination.js
+++ src/ui/components/Pagination/Pagination.js
@@ -1,23 +1,32 @@
 import React from 'react';
 // import classnames from 'classnames';
 import { Link } from '@components/Link';
+import { Button } from '@components/Button';
 // import styles from './Pagination.module.scss';
 
 const Pagination = ({ path, currentPage, limit, numPages }) => {
-  const linkPrev = `${path}/${currentPage - 1}`;
+  const linkPrev =  (currentPage == 2)
+    ? `${path}`
+    : `${path}/${currentPage - 1}`;
   const linkNext = `${path}/${currentPage + 1}`;
+  const disablePrev = (currentPage == 1)
+    ? true
+    : false;
+  const disableNext = (currentPage == numPages)
+    ? true
+    : false;
 
-  return (
+
+  return (numPages != 1) ? (
     <div>
-      Pagination
       <Link to={linkPrev}>
-        Prev
+        <Button disabled={disablePrev}>Prev</Button>
       </Link>
       <Link to={linkNext}>
-        Next
+        <Button disabled={disableNext}>Next</Button>
       </Link>
     </div>
-);
+  ) : null;
 };
 
 export default Pagination;
